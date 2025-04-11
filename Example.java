fe569f988113d0d11d9ffbead5fdddbfba65faba
assusmd <smdasrar07@gmail.com>
2025-04-11 18:54:07 +0530

    "Resolving merge conflicts"

--- Example.java
+++ Example.java
@@ -2,5 +2,6 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("main branch commit");
         System.out.println("This is in story branch.");
+        System.out.println("This is another commit in main branch.");
     }
 }
