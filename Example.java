1232ce2b821edab2f0b2c34b7f27b59642b43b83
assusmd <smdasrar07@gmail.com>
2025-04-11 18:58:08 +0530

    "story commit 3"

--- Example.java
+++ Example.java
@@ -3,5 +3,6 @@ public class Example {
         System.out.println("main branch commit");
         System.out.println("This is in story branch.");
         System.out.println("This is another story commit.");
+        System.out.println("This is another story commit 2");
     }
 }
